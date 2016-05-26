5ccc0c8ba5725899c75b2cf7db26baa988a46c47
Wang YongHua <wangyonghua@patsnap.com>
2016-05-26 13:02:31 +0800

    add monent format

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,14 +1,28 @@
-let datePicker = require('../js/lib/picker.date.js')
-let _template = `<section><input placeholder="{{placeholder}}" v-el:date type="date" class="datepicker"> </section>`;
-
-
+require('../js/lib/picker.date.js')
+let _template = `<section><input  placeholder="{{placeholder}}" v-el:date type="date" class="datepicker"> </section>`;
 export default {
     template: _template,
     props: {
-        placeholder:String
+        placeholder:String,
+        val:Number,
+        setDate: Function
+    },
+    methods:{
+        setAllDate(){
+            console.log('mmmm Name');
+        }
     },
     ready(){
+        let vm = this;
         $(this.$els.date).pickadate({
+            closeOnSelect: true,
+            onSet: function(context) {
+                vm.val = context.select;
+                console.log('Just set stuff:', context)
+            },
+            onClose: function(context) {
+                console.log('Just close:', context)
+            },
             selectMonths: true, // Creates a dropdown to control month
             selectYears: 15 // Creates a dropdown of 15 years to control year
         });
--- src/components/todoOne.js
+++ src/components/todoOne.js
@@ -1,3 +1,7 @@
+import _ from 'lodash';
+import performance from '../js/performance_mixin';
+import datePicker from './calendar';
+import moment from 'moment';
 let template = `
 <section  style="-webkit-user-select: none;padding: 10px 20px;">
     <div class="m-media">
@@ -9,18 +13,18 @@ let template = `
             <div v-show="isEditMode || !todoId" >
                 <input
                     placeholder="todo content"
-                    v-el:editinput :autofocus="!todoId" type="text" @click.stop v-model="todoItem.text" lazy />
+                    v-el:editinput :autofocus="!todoId" type="text" @keyup.enter="saveTodo" @click.stop v-model="todoItem.text" lazy />
             </div>
             <div
                 :class={'done-todo':todoItem.done}
                 v-else
             >
                 <div class="todoItem-title">{{todoItem.text}}</div>
-                <div class="todoItem-time">{{todoItem.time || (new Date())}}</div>
+                <div class="todoItem-time">{{todoItem.time || (new Date()) | formatDate}}</div>
              </div>
         </div>
         <div class="media-right media-middle">
-            <span class="icon-time"><date-picker placeholder="reminder time"></date-picker></span>
+            <span class="icon-time"><date-picker :set-Date="saveTodo" :val.sync="todoItem.time" placeholder="reminder time"></date-picker></span>
         </div>
     </div>
     <div class="row" style="margin-bottom:0px;display:none">
@@ -46,9 +50,6 @@ let template = `
         <div class="col s2"><span class="icon-time"></span></div>
     </div>
 </section>`;
-import _ from 'lodash';
-import performance from '../js/performance_mixin';
-import datePicker from './calendar';
 
 const initTodo = {
     id: null,
@@ -79,21 +80,22 @@ export default {
         };
     },
     watch:{
-        todoItem:{
-            deep: true,
-            //这地方逻辑处理的比较耦合，跟todolist 中的wath Listdata，不好理解
-            handler(newVal){
-                // debugger
-                if (!_.isEmpty(newVal.text)) {
-                    this.$dispatch('set-todo', this.todoId, newVal);
-                    this.disableEdit();
-                }else {
-                    this.$els.editinput.focus();
-                }
-            }
+        'todoItem.done':function (){
+            this.saveTodo();
+        },
+        'todoItem.time':function (val){
+            this.todoItem.time = val;
         }
     },
     methods:{
+        saveTodo(){
+            if (!_.isEmpty(this.todoItem.text)) {
+                this.$dispatch('set-todo', this.todoId, this.todoItem);
+                this.disableEdit();
+            }else {
+                this.$els.editinput.focus();
+            }
+        },
         enableEdit() {
             if (this.todoItem.done) {
                 return;
@@ -119,6 +121,13 @@ export default {
     },
     components: {
         datePicker
+    },
+    filters:{
+        formatDate(val, format){
+            let mt = moment(val);
+            return mt.format(format||'YYYY-MM-DD HH:mm')
+        }
+
     },
     directives: {
         placeholder(placeString) {
